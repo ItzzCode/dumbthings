fab3c4b1f731b8680e05a58699db8e4f4865aa8b
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-10-19 18:28:35 -0400

    Create littlepy.py

---
+++ python/littlepy.py
@@ -0,0 +1,59 @@
+#littlepy
+#it compresses some python commands into a letter or so
+#dumb
+
+inp=input("> ")
+#registries
+r=0
+a=0
+b=0
+c=0
+d=0
+e=0
+
+#commands:
+#a-e: save r to a through e
+#A-E save a-e to r 
+#?: send input to r
+#=: save the result of if a == b to c
+#+: add a and b, save to c
+#-: subtract a and b, save to c
+#*: multiply a and b, save to c
+#/: divide a and b, save to c
+#!: prints r
+
+for i in inp:
+    if i=="a":
+        a=r
+    if i=="b":
+        b=r
+    if i=="c":
+        c=r
+    if i=="d":
+        d=r
+    if i=="e":
+        e=r
+    if i=="A":
+        r=a
+    if i=="B":
+        r=b
+    if i=="C":
+        r=c
+    if i=="D":
+        r=d
+    if i=="E":
+        r=e
+    if i=="?":
+        c=input("?")
+    if i=="=":
+        c=a==b
+    if i=="+":
+        c=a+b
+    if i=="-":
+        c=a-b
+    if i=="*":
+        c=a*b
+    if i=="/":
+        c=a/b
+    if i=="!":
+        print(r)
