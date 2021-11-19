58d6c22c4be5756c3cb8f785ee38c1033d6a6f34
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-11-19 11:57:39 -0500

    dumb stuff classy functions, and untested

---
+++ python/pythings/main.py
@@ -0,0 +1,13 @@
+import pythingsfuncs as f
+
+xmax, ymax = 10
+scrmem, mem = f.misc.createList(30)
+
+def main():
+    f.display.displayScreen()
+    return 0
+
+if __name__ == "__main__":
+    main()
+else:
+    print("huh")
---
+++ python/pythings/pythingsfuncs.py
@@ -0,0 +1,11 @@
+class display():
+    def displayScreen():
+        pass #TODO
+
+class misc():
+    def createList(length,fill=""):
+        return list(fill*length)
+
+
+if __name__ == "__main__":
+    print("Run main, lol.")
